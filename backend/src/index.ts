14073249980876a1e3377c7c859e66346700fb7b
arnav <arnabdas2753@gmail.com>
2024-10-31 01:37:43 +0530

    push stage 1

---
+++ backend/src/index.ts
@@ -0,0 +1,13 @@
+import { WebSocketServer } from 'ws';
+import { GameManager } from './GameManager';
+
+const port = 8080
+const wss = new WebSocketServer({ port});
+
+const gameManager = new GameManager();
+
+wss.on('connection', function connection(ws) {
+    gameManager.addUser(ws)
+  ws.on('disconnect', ()=> gameManager.removeUser(ws));
+
+});
